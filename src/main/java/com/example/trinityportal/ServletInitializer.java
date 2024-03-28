0356989eff673dc597bd8a0015d2f437530809a0
js.kim <js.kim@dfocus.net>
2024-03-28 19:53:47 +0900

    feat: first Commit - SpringBoot 프로젝트 - thymeleaf 종속성 추가

---
+++ src/main/java/com/example/trinityportal/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.trinityportal;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TrinityportalApplication.class);
+	}
+
+}
---
+++ src/main/java/com/example/trinityportal/TrinityportalApplication.java
@@ -0,0 +1,13 @@
+package com.example.trinityportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrinityportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrinityportalApplication.class, args);
+	}
+
+}
